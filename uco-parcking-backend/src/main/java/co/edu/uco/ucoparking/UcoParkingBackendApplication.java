64f4ef3f7118f345b909e8b28e4ab2e80f2c2d6e
yeisonvargas01 <yeisonvargass27@gmail.com>
2026-05-14 18:15:59 -0500

    Prueba con REST

---
+++ uco-parcking-backend/src/main/java/co/edu/uco/ucoparking/UcoParkingBackendApplication.java
@@ -0,0 +1,12 @@
+package co.edu.uco.ucoparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcoParkingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcoParkingBackendApplication.class, args);
+	}
+}
